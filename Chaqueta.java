8fa4e8ab7dbe6c5cfbcd12c3ba521c296cb38e8b
Patricia López <patlopsev@alu.edu.gva.es>
2024-11-27 17:18:24 +0100

    añadir constructores, getters, setters y impresión

--- Chaqueta.java
+++ Chaqueta.java
@@ -1,3 +1,79 @@
 public class Chaqueta {
+    private String tipo;
+    private String talla;
+    private int cantidad;
+    private double precio;
+    private String color;
+
+
+    public Chaqueta() {
+    }
+
+
+    public Chaqueta(String tipo, String talla, int cantidad, double precio, String color) {
+        this.tipo = tipo;
+        this.talla = talla;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.color = color;
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+    public String getTalla() {
+        return talla;
+    }
+
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+
+    public double getPrecio() {
+        return precio;
+    }
+
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String imprimirDetalles () {
+
+        return "La chaqueta es de tipo " + tipo + ", de la talla: " + talla + ", del color: " + color + ", existe un stock de: " +  cantidad + ", con un precio de: " + precio + "euros";
+
+    }
+
+    
     
 }
--- Pantalon.java
+++ Pantalon.java
@@ -1,3 +1,68 @@
 public class Pantalon {
+    private String tipo;
+    private String talla;
+    private int cantidad;
+    private double precio;
+    private String color;
+
+    public Pantalon() {
+    }
+
+    public Pantalon(String tipo, String talla, int cantidad, double precio, String color) {
+        this.tipo = tipo;
+        this.talla = talla;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String imprimirDetalles () {
+
+        return "El pantalón es de tipo " + tipo + ", de la talla: " + talla + ", del color: " + color + ", existe un stock de: " +  cantidad + ", con un precio de: " + precio + "euros";
+
+    }
+
+
+    
     
 }
